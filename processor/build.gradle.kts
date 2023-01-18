06925347ffaed153d55c0fb57f3e4e0b327a0cb7
Anton Efimchuk <anton.efimchuk@gmail.com>
2023-01-18 19:28:29 +0100

    Add some tests

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -6,5 +6,12 @@ dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.7.21-1.0.8")
     implementation("com.squareup:kotlinpoet-ksp:1.12.0")
     implementation(project(":runtime"))
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.9")
+    testImplementation(kotlin("test"))
+    testImplementation("com.google.devtools.ksp:symbol-processing:1.7.21-1.0.8")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
---
+++ processor/src/test/kotlin/me/qnox/builder/processor/ProcessorTest.kt
@@ -0,0 +1,68 @@
+package me.qnox.builder.processor
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspWithCompilation
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+class ProcessorTest {
+
+    @Test
+    fun `test builder annotation processor`() {
+        val result = compile(
+            SourceFile.kotlin(
+                "KClass.kt",
+                """
+                    import me.qnox.builder.Builder
+        
+                    @Builder
+                    class KClass(val attr: Int)
+                """,
+            ),
+        )
+        val generatedBuilder = result.classLoader.loadClass("KClassBuilder")
+        generatedBuilder.getDeclaredMethod("attr", Int::class.java).shouldNotBeNull()
+    }
+
+    @Test
+    fun `test builder annotation processor with builder attr type`() {
+        val result = compile(
+            SourceFile.kotlin(
+                "KClass.kt",
+                """
+                    import me.qnox.builder.Builder
+    
+                    @Builder
+                    class KClass(val attr: KAttrClass)
+                """,
+            ),
+            SourceFile.kotlin(
+                "KAttrClass.kt",
+                """
+                    import me.qnox.builder.Builder
+    
+                    @Builder
+                    class KAttrClass(val attr: Int)
+                """,
+            ),
+        )
+        val generatedBuilder = result.classLoader.loadClass("KClassBuilder")
+        generatedBuilder.getDeclaredMethod("attr", Function1::class.java).shouldNotBeNull()
+    }
+
+    private fun compile(vararg typeSource: SourceFile): KotlinCompilation.Result {
+        val compilation = KotlinCompilation().apply {
+            sources = listOf(*typeSource)
+            symbolProcessorProviders = listOf(ProcessorProvider())
+            inheritClassPath = true
+            kspWithCompilation = true
+        }
+        val result = compilation.compile()
+
+        result.exitCode.shouldBe(KotlinCompilation.ExitCode.OK)
+        return result
+    }
+}
